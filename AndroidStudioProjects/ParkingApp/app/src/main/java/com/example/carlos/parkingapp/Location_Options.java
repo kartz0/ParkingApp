760d35e3cd338c1da4c37f58097dd449b43a3643
Carlos <carl05v@hotmail.com>
2016-11-15 21:37:55 -0800

    full commit

---
+++ AndroidStudioProjects/ParkingApp/app/src/main/java/com/example/carlos/parkingapp/Location_Options.java
@@ -0,0 +1,18 @@
+package com.example.carlos.parkingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Location_Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location__options);
+
+        Intent intent = getIntent();
+
+
+    }
+}
